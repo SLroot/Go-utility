44e6210c4b3c73ce99fb893e435bd09b8b8f429c
slroot <saeid17lotfi@gmail.com>
2025-01-13 04:08:06 +0330

    first thing

---
+++ utility.go
@@ -0,0 +1,25 @@
+package goutility
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func IntInputFromUser() int {
+	fmt.Print("enter your number: ")
+	iinput := bufio.NewReader(os.Stdin)
+	in, err := iinput.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	input := strings.Replace(in, "\n", "", 1)
+	intInput, err := strconv.Atoi(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return intInput
+}
